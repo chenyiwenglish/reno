7ae6239ba76be37807c468e46643732b07dfb29d
daydream1ng <loveyy1203@163.com>
2019-07-28 15:27:14 +0800

    StartSpring初始化项目

---
+++ src/main/java/com/chenyiwenglish/reno/RenoApplication.java
@@ -0,0 +1,13 @@
+package com.chenyiwenglish.reno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chenyiwenglish/reno/RenoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenyiwenglish.reno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
