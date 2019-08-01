80ae8ffc8283b804a8280e03747ba1b67c4ebb6e
chenyiwenglish <loveyy1203@163.com>
2019-08-01 22:16:03 +0800

    增加redis

--- reno-api/src/main/java/com/chenyiwenglish/reno/vo/message/MessageVo.java
+++ reno-api/src/main/java/com/chenyiwenglish/reno/vo/message/MessageVo.java
@@ -1,10 +1,12 @@
 package com.chenyiwenglish.reno.vo.message;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class MessageVo {
     private Long id;
     private String name;
--- reno-biz/src/main/java/com/chenyiwenglish/reno/biz/handle/impl/MessageBizHandleImpl.java
+++ reno-biz/src/main/java/com/chenyiwenglish/reno/biz/handle/impl/MessageBizHandleImpl.java
@@ -1,5 +1,6 @@
 package com.chenyiwenglish.reno.biz.handle.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.chenyiwenglish.reno.biz.handle.MessageBizHandle;
 import com.chenyiwenglish.reno.dal.dao.MessageDao;
 import com.chenyiwenglish.reno.dal.model.Message;
@@ -7,19 +8,30 @@ import com.chenyiwenglish.reno.vo.message.GetMessageListRequest;
 import com.chenyiwenglish.reno.vo.message.GetMessageListResponse;
 import com.chenyiwenglish.reno.vo.message.MessageVo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @Component
 public class MessageBizHandleImpl implements MessageBizHandle {
 
+    private final String MESSAGE_LIST_CACHE_KEY = "msg:list";
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @Autowired
     private MessageDao messageDao;
 
     @Override
+    @SuppressWarnings("unchecked")
     public GetMessageListResponse getMessageList(GetMessageListRequest request) {
+        if (redisTemplate.hasKey(MESSAGE_LIST_CACHE_KEY)) {
+            return JSON.parseObject((String) redisTemplate.opsForValue().get(MESSAGE_LIST_CACHE_KEY), GetMessageListResponse.class);
+        }
         GetMessageListResponse response = new GetMessageListResponse();
         List<Message> messages = messageDao.getMessageList();
         response.setMessages(messages.stream()
@@ -28,6 +40,7 @@ public class MessageBizHandleImpl implements MessageBizHandle {
                         .name(message.getName())
                         .content(message.getContent()).build())
                 .collect(Collectors.toList()));
+        redisTemplate.opsForValue().set(MESSAGE_LIST_CACHE_KEY, JSON.toJSONString(response), 60, TimeUnit.SECONDS);
         return response;
     }
 }
